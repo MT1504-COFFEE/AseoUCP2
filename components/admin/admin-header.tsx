9e3d4827685e2f8a023a367f596a65cfe9b8a153
Mathius  Lozano <mathius.lozano@ucp.edu.co>
2025-11-03 21:27:35 -0500

    CAmbios en el header ADMIN

--- components/admin/admin-header.tsx
+++ components/admin/admin-header.tsx
@@ -9,17 +9,19 @@ export function AdminHeader() {
   const { user, logout } = useAuth()
 
   return (
-    <header className="border-b bg-card" >
+    <header className="border-b bg-card">
       <div className="container mx-auto px-4 py-4">
-        <div className="flex items-center justify-between">
-          <div className="flex items-center gap-3">
-           <div className="flex justify-center mb-4">
-    <img
-      src="/ImagenUniversidadCatolicadePereira.png" 
-      alt="Logo del Sistema"
-      className="h-16 w-auto"
-    />
-    </div>
+      <div className="flex items-center justify-between">
+        <div className="flex items-center gap-3">
+          <div className="flex justify-center mb-4">
+        <div className="aspect-square h-14">
+          <img
+            src="/ImagenUniversidadCatolicadePereira.png"
+            alt="Logo del Sistema"
+            className="object-contain w-full h-full"
+          />
+        </div>
+      </div>
             <div>
               <h1 className="text-xl font-bold ">Sistema de Limpieza</h1>
               <p className="text-sm text-muted-foreground">Panel de Administración</p>
